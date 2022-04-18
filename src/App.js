99402a694c0920abe1e71b7630e3ce754987aced
alssel2525 <alssel2525@naver.com>
2022-04-19 07:25:51 +0900

    헤더, 푸터 작성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import logo from "./images/logo.png";
+import logo_white from "./images/logo_white.png";
+import naver_white from "./images/naver_white.png";
+import kakaotalk_white from "./images/kakaotalk_white.png";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<header>
+				<div className={"header__container"}>
+					<a href={"/#"}><img src={logo} alt={"Logo"}/></a>
+					<nav>
+						<ul>
+							<a href={"/#"}><li>회사소개</li></a>
+							<a href={"/#"}><li>팀원소개</li></a>
+							<a href={"/#"}><li>오시는길</li></a>
+							<a href={"/#"}><li>인재채용</li></a>
+							<a href={"/#"}><li>광고제휴</li></a>
+							<a href={"/#"}><li>공시정보</li></a>
+						</ul>
+					</nav>
+				</div>
+			</header>
+			<main>
+			
+			</main>
+			<footer>
+				<div className={"footer__container"}>
+					<div className={"footer__1"}>
+						<a href={"/#"}><img src={logo_white} alt={"Logo"}/></a>
+						<nav>
+							<ul>
+								<a href={"/#"}><li>광고제휴</li></a>
+								<a href={"/#"}><li>인재채용</li></a>
+								<a href={"/#"}><li>이용약관</li></a>
+								<a href={"/#"}><li>개인정보처리방침</li></a>
+							</ul>
+						</nav>
+					</div>
+					<div className={"footer__2"}>
+						<span>
+							(주)넛지헬스케어 / CashWalk, Inc.<br/>
+							서울특별시 강남구 역삼로1길 8(역삼동, 평익빌딩) / 전화 02-0000-0000 / 이메일 cs@cashwalk.io<br/>
+							Copyright © 2022 by CashWalk, Inc. All Rights Reserved.
+						</span>
+						<div className={"footer__2__logos"}>
+							<a href={"/#"}><img src={naver_white} alt={"네이버"}/></a>
+							<a href={"/#"}><img src={kakaotalk_white} alt={"카카오톡"}/></a>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</>
+	);
 }
 
 export default App;
